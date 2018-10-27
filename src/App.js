d1fd547918cf09e5d6ab82042515a78eafef544e
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-10-27 21:19:41 +0530

    Setup react-router + routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import classNames from "classnames";
+import "./App.css";
+
+const ProfilePage = () => <h1>Profile</h1>;
+
+const PostPage = () => <h1>Post</h1>;
+const MessagePage = () => <h1>Message</h1>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hello Trustvocate</h1>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" render={() => <ProfilePage />} />
+          <Route path="/post" render={() => <PostPage />} />
+
+          <Route path="/message" render={() => <MessagePage />} />
+        </div>
+      </Router>
     );
   }
 }
