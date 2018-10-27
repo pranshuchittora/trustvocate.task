256325d2d4e734f81877bdfbe144883356d1ce91
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-10-27 22:07:42 +0530

    Updates

---
+++ precache-manifest.c45807abdb8409e1a9a5213b0a83509e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0785d90c48a13a47675",
+    "url": "/trustvocate.task/static/js/runtime~main.f0785d90.js"
+  },
+  {
+    "revision": "19d48307c374ef0a0b77",
+    "url": "/trustvocate.task/static/js/main.19d48307.chunk.js"
+  },
+  {
+    "revision": "80482514d6b12468bd0c",
+    "url": "/trustvocate.task/static/js/1.80482514.chunk.js"
+  },
+  {
+    "revision": "19d48307c374ef0a0b77",
+    "url": "/trustvocate.task/static/css/main.648a75bc.chunk.css"
+  },
+  {
+    "revision": "772d697ef657c6140903783fe779d25f",
+    "url": "/trustvocate.task/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/trustvocate.task/precache-manifest.5927c60d80bdd068976dbc787f34ab2e.js"
+  "/trustvocate.task/precache-manifest.c45807abdb8409e1a9a5213b0a83509e.js"
 );
 
 workbox.clientsClaim();
