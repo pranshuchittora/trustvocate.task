a063a19f557b127ac097ec81766b305817f5995d
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-10-30 15:36:35 +0530

    Left Right messages works :D

--- src/Pages/MessagePage/MessagePage.js
+++ src/Pages/MessagePage/MessagePage.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
-
+import classNames from "classnames";
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 // Custom Imports
+import localStyles from "./MessagePage.css";
 import __NavBar from "../../components/NavBar/NavBar";
 import __MessageList from "../../components/MessageList/MessageList";
+import SendIcon from "@material-ui/icons/SendOutlined";
 
 class ProfilePage extends React.Component {
   state = {
     InputA: "",
+    InputB: "",
     messages: [
       {
         senderId: "Person A",
-        text: "Me myself"
-      },
-      {
-        senderId: "Person A",
-        text: "XYZ?"
+        text: "Hi Mr.B"
       }
     ]
   };
@@ -25,27 +25,80 @@ class ProfilePage extends React.Component {
   handleInputA = e => {
     this.setState({ InputA: e.target.value });
   };
-  add = message => {
+
+  handleInputB = e => {
+    this.setState({ InputB: e.target.value });
+  };
+  addA = () => {
     let newArr = this.state.messages.concat({
       senderId: "Person A",
       text: this.state.InputA
     });
 
-    this.setState({messages:newArr})
+    this.setState({ messages: newArr });
+  };
+
+  addB = () => {
+    let newArr = this.state.messages.concat({
+      senderId: "Person B",
+      text: this.state.InputB
+    });
+
+    this.setState({ messages: newArr });
   };
   render() {
     return (
-      <div>
+      <div >
         <__NavBar />
-        <Typography variant="h1">Messages</Typography>
-        <__MessageList messages={this.state.messages}  />
-        <TextField
-          id="standard-dense"
-          placeholder="Message - Person A"
-          onInput={event => this.handleInputA(event)}
-          margin="dense"
-        />
-        <Button onClick={this.add}>+</Button>
+        <Typography variant="h2" style={{padding:50,textAlign:"center"}}>Chat 101</Typography>
+
+        <Grid container justify="center" spacing={24}>
+          <Grid
+            item
+            xs={12}
+            md={9}
+            className={classNames(localStyles.messagesContRoot)}
+          >
+            <__MessageList
+              messages={this.state.messages}
+              className={classNames(localStyles.messageList)}
+            />
+            <div className={classNames(localStyles.inputContRoot)}>
+              <div className={classNames(localStyles.inputCont)}>
+                <TextField
+                  id="input-person-A"
+                  placeholder="Message - Person A"
+                  onInput={event => this.handleInputA(event)}
+                  margin="dense"
+                />
+                <Button
+                  color="primary"
+                  variant="raised"
+                  className={classNames(localStyles.sendBtn)}
+                  onClick={this.addA}
+                >
+                  <SendIcon />
+                </Button>
+              </div>
+              <div className={classNames(localStyles.inputCont)}>
+                <TextField
+                  id="input-person-B"
+                  placeholder="Message - Person B"
+                  onInput={event => this.handleInputB(event)}
+                  margin="dense"
+                />
+                <Button
+                  color="primary"
+                  variant="raised"
+                  className={classNames(localStyles.sendBtn)}
+                  onClick={this.addB}
+                >
+                  <SendIcon />
+                </Button>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,22 +1,31 @@
 import React from "react";
-
+import classNames from "classnames";
+import localStyles from "./MessageList.css";
 class MessageList extends React.Component {
   render() {
     return (
- <div>
+      <div>
         {this.props.messages.map(message => {
           return (
-              <div style={{padding:10,background:"#ccc",margin:20}}>
-            <div key={message.id}>
-              <div>{message.senderId}</div>
-              <div>{message.text}</div>
-            </div>
+            <div
+              className={classNames(
+                message.senderId == "Person B"
+                  ? localStyles.right
+                  : localStyles.left,
+                localStyles.message
+              )}
+              style={{ padding: 10, margin: 20 }}
+            >
+              <div key={message.id}>
+                <div>{message.senderId}</div>
+                <div>{message.text}</div>
+              </div>
             </div>
           );
         })}
-     </div>
+      </div>
     );
   }
 }
 
-export default MessageList
+export default MessageList;
